21fcecc71438d1d0833ee5ea4e826583258c757a
PseudoCho <andrew.cho1992@gmail.com>
2020-04-24 04:03:41 +1000

    Left/Right input movement + updated sprite
    Input works for onkeydown but there is slight delay at start, use https://forum.jquery.com/topic/how-to-move-an-element-with-arrow-keys-consistently
    
    Sprite updated with mirrored positions

--- LD46.js
+++ LD46.js
@@ -1,10 +1,18 @@
 //import * as animate from './animate.js';
 //import * as lol from './helper.js';
 
-var x, y, srcx, srcy, sheetWidth, sheetHeight, frameColumn, width, height, currentFrame;
+// left = 37
+// up = 38
+// right = 39
+// down = 40
+
+var x, y, srcx, srcy, sheetWidth, sheetHeight, frameColumn, width, height;//, currentFrame;
 var canWidth, canHeight;
 var clownImage;
 var ctx;
+var frame0 = 44;
+var frame1 = 88;
+var frame2 = 132;
 
 function initialise() {
     //var preload = document.createElement("CANVAS");
@@ -20,7 +28,7 @@ function initialise() {
     sheetWidth = 108;
     sheetHeight = 51;
     frameColumn = 3;
-    width = 44;
+    width = frame0;
     height = 51;
     
     // set x by canvas.width - sprite.width then divide by 2
@@ -28,7 +36,7 @@ function initialise() {
     y = canHeight - height;
     //width = sheetWidth / frameColumn;
     //height = sheetHeight; // usually have frameRow but here it's 1
-    currentFrame = 0;
+    //currentFrame = 0;
     startGame();
 }
 
@@ -44,19 +52,47 @@ function startGame() {
     draw();
 }
 
-function updateFrame() {
-    currentFrame = ++currentFrame % frameColumn;
-    srcx = currentFrame * width;
-    srcY = 0;
+function updateFrame(leftOrRight) {
+    if(leftOrRight === 37) {
+        width = frame1;
+        x -= 5;
+    } else if(leftOrRight === 39) {
+        width = frame2;
+        x += 5;
+    }
+    // to make smooth movement (first increment delay) we use:
+    // Animate() is not what you want for smooth moves.  If you use .css to move one pixel at a time, start it up when the key is pressed, and stop when the key is released. You get smoothness.
+    // https://forum.jquery.com/topic/how-to-move-an-element-with-arrow-keys-consistently
+
+    ctx.drawImage(clownImage, srcx, srcy, width, height, x, y, width, height);
+    
+    // currentFrame = ++currentFrame % frameColumn;
+    
+    //srcx = currentFrame * width;
+    // srcy = 0; // srcy is always 0
 }
 
 function draw() {
    // updateFrame();
-   if(!clownImage.complete) {
-       setTimeout(function() {
-           draw();
-       }, 50);
-   }
-   ctx.drawImage(clownImage, srcx, srcy, width, height, x, y, width, height);
-   //ctx.drawImage(clownImage, srcx, srcy, width, height, x, y, width, height);
+    if(!clownImage.complete) {
+        setTimeout(function() {
+            draw();
+        }, 50);
+    }
+    ctx.drawImage(clownImage, srcx, srcy, width, height, x, y, width, height);
+    
+    //ctx.drawImage(clownImage, srcx, srcy, width, height, x, y, width, height);
 }
+
+document.onkeydown = function(event) {
+    switch(event.keyCode) {
+       case 37:
+          //alert('left key'); // call function updateFrame(left)?
+            updateFrame(37);
+         break;
+       case 39:
+          //alert('right key'); // call function updateFrame(right)?
+            updateFrame(39);
+         break;
+    }
+};
